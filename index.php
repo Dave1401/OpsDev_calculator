ab29f03d2a1a6bc7f62af24b30b55be083d5450e
Dave <71814869+Dave1401@users.noreply.github.com>
2021-01-07 14:04:47 +0100

    Update index.php
    naam van vermenigvuldigen veranderd

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             </div>
             <button type="submit" class="btn btn-primary" name="add">Add</button>
             <button type="submit" class="btn btn-primary" name="subtract">Subtract</button>
-            <button type="submit" class="btn btn-primary" name="multiply">Multiply</button>
+            <button type="submit" class="btn btn-primary" name="multiply">maal</button>
         </form>
         <main>
         <?php
