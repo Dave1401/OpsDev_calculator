42f301fcf9ec4d0b6e0c551e476c5bb8af4eb2a0
Dave <71814869+Dave1401@users.noreply.github.com>
2021-01-07 14:34:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
             <button type="submit" class="btn btn-primary" name="add">Add</button>
             <button type="submit" class="btn btn-primary" name="subtract">Subtract</button>
             <button type="submit" class="btn btn-primary" name="multiply">Multiply</button>
-            <button type="submit" class="btn btn-primary" name="Delen">deel</button>
+            <button type="submit" class="btn btn-primary" name="delen">delen</button>
         </form>
         <main>
         <?php
